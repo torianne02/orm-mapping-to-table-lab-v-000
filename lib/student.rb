489067660eda7895d4cc1dabe231fb82d2bb0a4e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-29 00:11:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,8 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 
   def self.create
