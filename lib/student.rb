634e0a0c442a05083141130d6bd1b554522a53b8
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-29 00:00:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
+  attr_accessor :name, :grade
 
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
