c5cbc224d115f689b670acabb7c1d3ffa868ebe7
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-29 00:05:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,10 +9,18 @@ class Student
   end
 
   def self.create_table
-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+      SQL
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
+
   end
 
   def save
