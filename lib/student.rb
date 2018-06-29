06dbf7eecca20d2466e1d977cee5163f0bc8c447
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-29 00:10:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,11 @@ class Student
   end
 
   def save
-
+    sql = <<-SQL 
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
   def self.create
